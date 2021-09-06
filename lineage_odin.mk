4bb0a33822bf8f4fe2c0a879ab36d4e8a73c8f15
MPortGroup <89680431+MPortGroup@users.noreply.github.com>
2021-09-06 23:27:36 +0800

    odin: Initial modification

--- lineage_odin.mk
+++ lineage_odin.mk
@@ -4,13 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-$(call inherit-product, device/xiaomi/venus/device.mk)
+$(call inherit-product, device/xiaomi/odin/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_venus
-PRODUCT_DEVICE := venus
+PRODUCT_NAME := lineage_odin
+PRODUCT_DEVICE := odin
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Mi 11
+PRODUCT_MODEL := 2106118C
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -8,7 +8,7 @@
 set -e
 
 # Required!
-export DEVICE=venus
+export DEVICE=odin
 export DEVICE_COMMON=sm8350-common
 export VENDOR=xiaomi
 
